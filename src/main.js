b3b320e17bed1d6876eb7fe9ebffa63b230ea051
zhaopengss123 <874391057@qq.com>
2020-06-03 23:27:29 +0800

    20200603开发

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { NavBar , Swipe, SwipeItem , Lazyload, DropdownMenu, DropdownItem, SwitchCell, Button, DatetimePicker } from 'vant';
+import { NavBar , Swipe, SwipeItem , Lazyload, DropdownMenu, DropdownItem, SwitchCell, Button, DatetimePicker, Field, Form, Popup,Picker,Dialog     } from 'vant';
 import 'lib-flexible/flexible'
 import Axios from './axios-conf';
 import stores from './store-conf';
+
 const store = stores(Vue);
 Axios(Vue, store);
 Vue.use(NavBar);
@@ -18,6 +19,11 @@ Vue.use(DropdownItem);
 Vue.use(SwitchCell);
 Vue.use(Button);
 Vue.use(DatetimePicker);
+Vue.use(Field);
+Vue.use(Form);
+Vue.use(Popup);
+Vue.use(Picker);
+Vue.use(Dialog);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/view/publish/index.vue
+++ src/view/publish/index.vue
@@ -1,85 +1,172 @@
 <template>
   <div class="index">
-    <van-nav-bar  left-arrow @click-left="backFun">
+    <van-nav-bar left-arrow @click-left="backFun">
       <template #title>
-            <ul class="nav-title-btn">
-                <li :class="toggleIndex === 1 ? 'active' : ''" @click="toggleIndex = 1; getDList(1)">
-                  <span>我要买</span>
-                  <i></i>
-                </li>
-                <li :class="toggleIndex === 2 ? 'active' : ''" @click="toggleIndex = 2; getDList(2)">
-                  <span>我要卖</span>
-                  <i></i>
-                </li>
-            </ul>    
-      </template> 
-      </van-nav-bar>
-      <div class="main">
-          <div class="list topList">
-              <ul>
-                <li>
-                  <div>选择道具</div>
-                  <div>
-                    <input class="top-input" type="text" placeholder="输入关键字搜索" />
-                    <img hidden src="../../assets/seach.png" alt=""></div>
-                </li>
-              </ul>
-          </div>
-          <div class="commodity-list">
-              <div :class="selectClass.id == item.id ? 'active' : '' " @click="selectFun(item)" v-for="(item,index) in commodityList" :key="index">
-                  <img :src="item.imgUrl" alt="" srcset="">
-              </div>
-          </div>
-          <div class="commodity-detail">
-            <div class="list">
-              <ul>
-                <li>
-                  <div>选择道具</div>
-                  <div>{{ selectClass.name }}</div>
-                </li>
-                <li>
-                  <div>选择道具</div>
-                  <div>{{ selectClass.name }}</div>
-                </li>
-                 <li>
-                  <div>标题描述</div>
-                  <div><input maxlength="30" type="text" placeholder="请输入"></div>
-                </li>
-                 <li>
-                  <div>直播间号</div>
-                  <div><input type="text" placeholder="请输入"></div>
-                </li>
-                <li>
-                  <div>主播姓名</div>
-                  <div><input maxlength="20" type="text" placeholder="请输入"></div>
-                </li>
-                <li style="padding:10px 0;">
-                  <div>配送时间</div>
-                  <div>
-                    <van-dropdown-menu style="width:100px; float:right; margin-right:2px;">
-                         <van-dropdown-item v-model="form1.deliveryTypeId" @change="reset" :options="deliveryList" />
-                    </van-dropdown-menu>    
-                  </div>
-                </li>
-                 <li>
-                  <div>求购金额(单价/份)</div>
-                  <div><input type="number" placeholder="请输入"></div>
-                </li>
-                <li>
-                  <div>数量</div>
-                  <div><input type="number" placeholder="请输入"></div>
-                </li>
-                <li>
-                  <div>合计金额</div>
-                  <div></div>
-                </li>       
-              </ul>
-          </div>
-          </div>
+        <ul class="nav-title-btn">
+          <li :class="toggleIndex === 1 ? 'active' : ''" @click="toggleIndex = 1; getDList(1)">
+            <span>我要买</span>
+            <i></i>
+          </li>
+          <li :class="toggleIndex === 2 ? 'active' : ''" @click="toggleIndex = 2; getDList(2)">
+            <span>我要卖</span>
+            <i></i>
+          </li>
+        </ul>
+      </template>
+    </van-nav-bar>
+    <div class="main">
+      <div class="list topList">
+        <ul>
+          <li>
+            <div>选择道具</div>
+            <div>
+              <input class="top-input" type="text" placeholder="输入关键字搜索" />
+              <img hidden src="../../assets/seach.png" alt />
+            </div>
+          </li>
+        </ul>
       </div>
-         
-     </div>
+      <div class="commodity-list">
+        <div
+          :class="selectClass.id == item.id ? 'active' : '' "
+          @click="selectFun(item)"
+          v-for="(item,index) in commodityList"
+          :key="index"
+        >
+          <img :src="item.imgUrl" alt srcset />
+        </div>
+      </div>
+    </div>
+    <div class="form1" v-if="toggleIndex === 1">
+      <van-form @submit="onSubmit1">
+        <van-field readonly v-model="selectClass.name" name="道具名" label="道具名" placeholder="请选择道具" />
+        <van-field
+          v-model="form1.roomNumber"
+          type="number"
+          name="直播间号"
+          label="直播间号"
+          placeholder="直播间号"
+          :rules="[{ required: true, message: '请填写直播间号' }]"
+        />
+        <van-field
+          v-model="form1.anchorName"
+          name="主播姓名"
+          label="主播姓名"
+          placeholder="主播姓名"
+          :rules="[{ required: true, message: '请填写主播姓名' }]"
+        />
+        <van-field
+          readonly
+          clickable
+          name="picker"
+          :value="form1.deliveryTypeName"
+          label="配送方式"
+          placeholder="点击选择配送方式"
+          @click="showPicker = true"
+        />
+        <van-field
+          v-model="form1.unitPrice"
+          type="number"
+          name="求购金额"
+          label="求购金额"
+          placeholder="求购金额(单价/份)"
+          :rules="[{ required: true, message: '请填写求购金额(单价/份)' }]"
+        />
+        <van-field
+          v-model="form1.stock"
+          type="number"
+          name="数量"
+          label="数量"
+          placeholder="数量"
+          :rules="[{ required: true, message: '请填写数量' }]"
+        />
+        <van-field
+          v-model="form1.describe"
+          name="道具描述"
+          label="道具描述"
+          placeholder="道具描述"
+          :rules="[{ required: true, message: '请填写道具描述' }]"
+        />
+        <div style="margin: 16px;">
+          <van-button class="sbtn" round block type="info" :loading="subLoading" native-type="submit">发布</van-button>
+        </div>
+      </van-form>
+    </div>
 
+    <div class="form2" v-if="toggleIndex === 1">
+      <van-form @submit="onSubmit2">
+        <van-field readonly v-model="selectClass.name" name="道具名" label="道具名" placeholder="请选择道具" />
+        <van-field
+          v-model="form2.titleDescribe"
+          name="标题描述"
+          label="标题描述"
+          placeholder="标题描述"
+          :rules="[{ required: true, message: '请填写标题描述' }]"
+        />
+        <van-field
+          v-model="form2.anchorName"
+          name="主播姓名"
+          label="主播姓名"
+          placeholder="主播姓名"
+          :rules="[{ required: true, message: '请填写主播姓名' }]"
+        />
+        <van-field
+          readonly
+          clickable
+          name="picker"
+          :value="form2.deliveryTypeName"
+          label="配送方式"
+          placeholder="点击选择配送方式"
+          @click="showPicker1 = true"
+        />
+        <van-field
+          v-model="form2.unitPrice"
+          type="number"
+          name="求购金额"
+          label="求购金额"
+          placeholder="求购金额(单价/份)"
+          :rules="[{ required: true, message: '请填写求购金额(单价/份)' }]"
+        />
+        <van-field
+          v-model="form2.stock"
+          type="number"
+          name="数量"
+          label="数量"
+          placeholder="数量"
+          :rules="[{ required: true, message: '请填写数量' }]"
+        />
+        <van-field
+          v-model="form2.describe"
+          name="道具描述"
+          label="道具描述"
+          placeholder="道具描述"
+          :rules="[{ required: true, message: '请填写道具描述' }]"
+        />
+        <div style="margin: 16px;">
+          <van-button class="sbtn" round block type="info" :loading="subLoading" native-type="submit">发布</van-button>
+        </div>
+      </van-form>
+    </div>
+
+
+    <van-popup v-model="showPicker" position="bottom">
+      <van-picker
+        show-toolbar
+        :columns="deliveryList"
+        @confirm="onConfirm"
+        @cancel="showPicker = false"
+      />
+    </van-popup>
+
+        <van-popup v-model="showPicker1" position="bottom">
+      <van-picker
+        show-toolbar
+        :columns="deliveryList"
+        @confirm="onConfirm1"
+        @cancel="showPicker1 = false"
+      />
+    </van-popup>
+  </div>
 </template>
 
 <script>
@@ -87,18 +174,107 @@ export default {
   name: "Index",
   data() {
     return {
+      subLoading: false,
       toggleIndex: 1,
       commodityList: [],
       parescommodityList: [],
       selectClass: {},
       showPicker: false,
+      showPicker1: false,
       deliveryList: [],
-      form1:{
-        deliveryTypeId: null
+      totalMoney: 0,
+      form1: {
+        deliveryTypeId: null,
+        deliveryTypeName: null,
+        describe: "",
+        roomNumber: null,
+        anchorName: null,
+        unitPrice: null,
+        stock: null,
+        describe: null
+      },
+      form2: {
+        deliveryTypeId: null,
+        deliveryTypeName: null,
+        describe: "",
+        roomNumber: null,
+        anchorName: null,
+        unitPrice: null,
+        titleDescribe: '',
+        stock: null,
+        describe: null
       }
     };
   },
   methods: {
+    onConfirm(val) {
+      this.form1.deliveryTypeId = val.id;
+      this.form1.deliveryTypeName = val.name;
+      this.showPicker = false;
+    },
+    // 求购发布
+    onSubmit1() {
+      this.subLoading = true;
+      this.axios
+        .post(`/release/buy`,{
+            propsConfigId: this.selectClass.id,
+            roomNumber: this.form1.roomNumber,
+            anchorName: this.form1.anchorName,
+            deliveryTypeId: this.form1.deliveryTypeId,
+            unitPrice: this.form1.unitPrice,
+            stock: this.form1.stock,
+            describe: this.form1.describe
+        })
+        .then(res => {
+                this.subLoading = false;
+          if (res.data.returnCode == "SUCCESS") {
+            Dialog({ message: '操作成功' });
+            setTimeout(res=>{
+              this.$router.push({ path: '/' })
+              done();
+            },1000)
+            
+          }else{
+            Dialog({ message:res.data.returnMsg });
+          }
+        })
+        .catch(error => {
+          this.subLoading = false;
+        });
+    },
+
+    // 售卖发布
+    onSubmit2() {
+      this.subLoading = true;
+      this.axios
+        .post(`/release/sell`,{
+            propsConfigId: this.selectClass.id,
+            roomNumber: this.form1.roomNumber,
+            anchorName: this.form1.anchorName,
+            deliveryTypeId: this.form1.deliveryTypeId,
+            unitPrice: this.form1.unitPrice,
+            stock: this.form1.stock,
+            describe: this.form1.describe
+        })
+        .then(res => {
+                this.subLoading = false;
+          if (res.data.returnCode == "SUCCESS") {
+            Dialog({ message: '操作成功' });
+            setTimeout(res=>{
+              this.$router.push({ path: '/' })
+              done();
+            },1000)
+            
+          }else{
+            Dialog({ message:res.data.returnMsg });
+          }
+        })
+        .catch(error => {
+          this.subLoading = false;
+        });
+    },
+
+
     backFun() {
       this.$router.back(-1);
     },
@@ -118,27 +294,24 @@ export default {
     selectFun(item) {
       this.selectClass = item;
     },
-    getDelivery(){
-         this.axios.post(`/delivery/all`).then(res => {
+    getDelivery() {
+      this.axios
+        .post(`/delivery/all`)
+        .then(res => {
           if (res.data.returnCode == "SUCCESS") {
-              let deliveryList = [{
-                text: '请选择',
-                value: null
-              }];
-              res.data.result.map(item=>{
-                item.value = item.id;
-                item.text = item.name;
-                deliveryList.push(item);
-              })
-              this.deliveryList = deliveryList;
+            let deliveryList = [];
+            res.data.result.map(item => {
+              item.value = item.id;
+              item.text = item.name;
+              deliveryList.push(item);
+            });
+            this.deliveryList = deliveryList;
           }
         })
         .catch(error => {
           //捕获失败
         });
-
     }
-    
   },
   mounted() {
     this.getDList(1);
@@ -150,14 +323,23 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .index {
-     .selectPicker {
-      width: 100%;
-      height: 260px;
-      position: fixed;
-      bottom: 0;
-      left: 0;
-      box-shadow: 0px 0px 15px #333;
-    }
+  .sbtn {
+    background: linear-gradient(
+      203deg,
+      rgba(248, 131, 39, 1) 0%,
+      rgba(224, 48, 91, 1) 100%
+    );
+    color: #fff;
+    border: none;
+  }
+  .selectPicker {
+    width: 100%;
+    height: 260px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    box-shadow: 0px 0px 15px #333;
+  }
   .nav-title-btn {
     width: 300px;
     overflow: hidden;
@@ -265,7 +447,6 @@ export default {
         }
       }
     }
- 
   }
 }
 </style>
